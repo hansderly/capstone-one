b81fce526ceeb7f923ec19af18b01f5d40d34711
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-04 11:50:18 -0400

    add hide button in speaker section mobile and refractor the js logic to be more shorter and more clear

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,18 +1,23 @@
 const menuIcon = document.querySelector('.menu_icon');
 const closeIcon = document.querySelector('.close_icon');
 const menu = document.querySelector('.navigation_wrapper');
+const buttonMore = document.querySelector('.button_more');
+const buttonHide = document.querySelector('.button_hide');
+const speakerSectionHide = document.querySelector('.speaker_hide');
 
-const openMenu = () => {
-	menu.style.display = 'flex';
-	menuIcon.style.display = 'none';
-	closeIcon.classList.remove('hide');
+const openCLoseMenu = () => {
+	menu.classList.toggle('hide--mobile');
+	menuIcon.classList.toggle('hide--mobile');
+	closeIcon.classList.toggle('hide');
 };
 
-const closeMenu = () => {
-	menu.style.display = 'none';
-	menuIcon.style.display = 'flex';
-	closeIcon.classList.add('hide');
+const showHideSpeaker = () => {
+	speakerSectionHide.classList.toggle('hide--mobile');
+	buttonMore.classList.toggle('hide--mobile');
+	buttonHide.classList.toggle('hide--mobile');
 };
 
-menuIcon.addEventListener('click', openMenu);
-closeIcon.addEventListener('click', closeMenu);
+menuIcon.addEventListener('click', openCLoseMenu);
+closeIcon.addEventListener('click', openCLoseMenu);
+buttonMore.addEventListener('click', showHideSpeaker);
+buttonHide.addEventListener('click', showHideSpeaker);
